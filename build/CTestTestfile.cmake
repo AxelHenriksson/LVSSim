bd0ad7220cdd6000d6254569d16b1bceb5262396
Axel Henriksson <Axe.henriksson@gmail.com>
2021-12-05 00:31:47 +0100

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/Programmering/CGraphics
-# Build directory: F:/Programmering/CGraphics/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestInstantiator "F:/Programmering/CGraphics/build/TestInstantiator.exe")
-set_tests_properties(TestInstantiator PROPERTIES  _BACKTRACE_TRIPLES "F:/Programmering/CGraphics/CMakeLists.txt;46;add_test;F:/Programmering/CGraphics/CMakeLists.txt;0;")
